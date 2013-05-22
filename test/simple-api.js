327325124e95f30b163c85bc1f2baa2a47cf9711
Xavier Via <fernando.via@gmail.com>
2013-05-22 19:36:24 -0300

    TCP works

---
+++ test/simple-api.js
@@ -0,0 +1,32 @@
+var jstp = require('../lib/jstp.js');
+
+var http = require('http');
+
+var server = http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+});
+
+jstp.listen({
+  tcp: 33333,
+  websocket: server
+});
+
+/*
+var obj = {
+  answer: function (dispatch) {
+    jstp.put({
+      resource: ["User", "Article"]
+    });
+  }
+}
+
+jstp.bind({
+  endpoint: {
+    method: "BIND",
+    resource: ["User", "*"]
+  }
+}, obj.answer, obj);
+*/
+
+server.listen(1337, '127.0.0.1');
---
+++ test/user.js
@@ -0,0 +1,7 @@
+var JSTP = require('../lib/jstp.js');
+
+JSTP.dispatch({
+  host: [['localhost', 33333, "tcp"]],
+  method: "PUT",
+  resource: ["User", "Xavier"]
+});
