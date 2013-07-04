516154f00be5a6f4f8b73708a5d0788f1387b982
Xavier Via <fernando.via@gmail.com>
2013-07-04 18:09:27 -0300

    Support for Answers both in Subscriptions and everywhere

---
+++ sample/answer/local/automatic/single/504.js
@@ -0,0 +1,26 @@
+var jstp = require("../../../../../index.js");
+
+var Resource = {
+  process: function (engine, answer, dispatch) {
+    console.log("Will timeout, not answer");
+  },
+  answer: function (engine, answer) {
+    console.log(answer);
+  }
+}
+
+jstp.bind({
+  endpoint: {
+    method: "*",
+    resource: ["User"]
+  }
+}, Resource.process, Resource);
+
+setTimeout( function () {
+  jstp.get({
+    resource: ["User"],
+    body: {
+      message: "Hello World"
+    }
+  }, Resource.answer, Resource);
+}, 1000)
---
+++ sample/answer/local/manually/single/200.js
@@ -0,0 +1,26 @@
+var jstp = require("../../../../../index.js");
+
+var Resource = {
+  process: function (engine, answer, dispatch) {
+    engine.answer(dispatch, 200, { message: "Ok" });
+  },
+  answer: function (engine, answer) {
+    console.log(answer);
+  }
+}
+
+jstp.bind({
+  endpoint: {
+    method: "*",
+    resource: ["User"]
+  }
+}, Resource.process, Resource);
+
+setTimeout( function () {
+  jstp.get({
+    resource: ["User"],
+    body: {
+      message: "Hello World"
+    }
+  }, Resource.answer, Resource);
+}, 1000)
