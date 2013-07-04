6498b1b6d8e30081b43f53390833a34e15da5c33
Xavier Via <fernando.via@gmail.com>
2013-07-04 15:26:10 -0300

    Sample: Subscription Websocket Two steps BIND

---
+++ sample/subscription/ws/two-steps/bind/method/put.js
@@ -0,0 +1,28 @@
+var jstp = require("../../../../../../index.js");
+
+var Resource =  {
+  userGet: function (engine, answer, dispatch) {
+    if (answer) console.log("The answer: " + answer.body);
+    else console.log(dispatch);
+  }
+}
+
+var host = [["localhost", 8000, "ws"], ["localhost", 9000, "ws"]];
+
+jstp.bind({
+  host: host,
+  endpoint: {
+    method: "PUT",
+    resource: ["User"],
+  }
+}, Resource.userGet, Resource);
+
+setTimeout( function () {
+  jstp.put({
+    host: [["localhost", 8000, "ws"], ["localhost", 9000, "ws"]],
+    resource: ["User"],
+    body: {
+      message: "Hello World"
+    }
+  });
+}, 1000);
