7a5cba16ce25f4342821556bdc25bbec5367a2c8
Xavier Via <fernando.via@gmail.com>
2013-07-04 15:33:04 -0300

    WS sample two steps release

---
+++ sample/subscription/ws/two-steps/release/resource/asterisk.js
@@ -0,0 +1,38 @@
+var jstp = require("../../../../../../index.js");
+
+var Resource =  {
+  userGet: function (engine, answer, dispatch) {
+    if (answer) console.log("The answer: " + answer.body);
+    else console.log(dispatch);
+  }
+}
+
+var host = [["localhost", 8000, "ws"], ["localhost", 9000, "ws"]];
+
+jstp.bind({
+  host: host,
+  endpoint: {
+    method: "DELETE",
+    resource: ["*"],
+  }
+}, Resource.userGet, Resource);
+
+setTimeout( function () {
+  jstp.release({
+    host: [["localhost", 8000, "ws"], ["localhost", 9000, "ws"]],
+    endpoint: {
+      method: "DELETE",
+      resource: ["*"],
+    }
+  }, Resource.userGet, Resource);
+  
+  setTimeout( function () {
+    jstp.delete({
+      host: [["localhost", 8000, "ws"], ["localhost", 9000, "ws"]],
+      resource: ["User"],
+      body: {
+        message: "Hello World"
+      }
+    });
+  }, 1000);
+}, 1000);
