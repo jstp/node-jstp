767bc65735fcf2a531b55d99ca45dc5b20ad4404
Luciano Bertenasco <lbertenasco@gmail.com>
2013-11-01 17:53:51 -0300

    Vows.js API Test: JSTPCallbackInterface

---
+++ spec/jstp/callback-interface.js
@@ -0,0 +1,9 @@
+var vows    = require('vows')
+  , assert  = require('assert')
+  , jstp    = require('../../index.js');
+
+vows.describe('JSTPCallbackInterface').addBatch({
+  '#call( JSTPEngine engine, JSTPTriggeringPackage triggeringPackage )': {
+    'should accept the arguments': 'pending'
+  }
+}).export(module); 
