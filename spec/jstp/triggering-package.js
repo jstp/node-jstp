aad0051a22225f586d5e1ece61dcb221b26470d8
Xavier Via <fernando.via@gmail.com>
2013-11-05 05:26:18 -0300

    JSTPTriggeringPackage: dispatch

--- spec/jstp/triggering-package.js
+++ spec/jstp/triggering-package.js
@@ -212,7 +212,7 @@ vows.describe('JSTPTriggeringPackage').addBatch({
     }
   },
 
-  '#answer( Integer statusCode, Object body [, JSTPCallback callback [, Object context ]])': {
+  '#answer( Integer statusCode, Object body [, JSTPCallable callback [, Object context ]])': {
     'there is a dispatch with transaction and triggering IDs': {
       'should prepare the JSTPDispatch answer and call the engine with the data': 'pending',
       'should set the From Header accordingly with the current Emitter': 'pending'
@@ -223,8 +223,122 @@ vows.describe('JSTPTriggeringPackage').addBatch({
     }
   },
 
-  '#dispatch( JSTPDispatch dispatch [, JSTPCallback callback [, Object context ]])': {
-    'should set the From Header accordingly with the current Emitter': 'pending'    
+  '#dispatch': {
+
+    'no arguments': {
+      'should raise a JSTPMissingDispatch exception': function () {
+        var triggeringPackage = new jstp.JSTPTriggeringPackage({}, "emitter");
+        assert.throws(function () {
+          triggeringPackage.dispatch();
+        }, jstp.JSTPMissingDispatch);
+      }
+    },
+
+    'at least an argument': {
+      'which is not a JSTPDispatch': {
+        'should throw a JSTPNotADispatch error': function () {
+          var triggeringPackage = new jstp.JSTPTriggeringPackage({}, "emitter");
+          assert.throws(function () {
+            triggeringPackage.dispatch("lala");
+          }, jstp.JSTPNotADispatch);
+        }
+      },
+
+      'there is something in the From Header': {
+        'should add the current emitter': function () {
+          var triggeringPackage = new jstp.JSTPTriggeringPackage(
+            { dispatch: function () {}}, "emitter");
+          var dispatch = new jstp.JSTPDispatch();
+          dispatch.setFrom(["there"]);
+          triggeringPackage.dispatch(dispatch);
+          assert.equal(dispatch.getFrom()[0], "emitter");
+          assert.equal(dispatch.getFrom()[1], "there");
+          assert.equal(dispatch.getFrom().length, 2);
+        }
+      },
+
+      'there is nothing in the From Header': {
+        'should set it to the current Emitter': function () {
+          var triggeringPackage = new jstp.JSTPTriggeringPackage(
+            {dispatch: function () {}}, "emitter");
+          var dispatch = new jstp.JSTPDispatch();
+          triggeringPackage.dispatch(dispatch);
+          assert.equal(dispatch.getFrom()[0], "emitter");
+          assert.equal(dispatch.getFrom().length, 1);          
+        }
+      },
+      
+      '( JSTPDispatch dispatch )': {
+        'should call the engine.dispatch with the dispatch': function () {
+          var theDispatch = new jstp.JSTPDispatch();
+
+          var mockEngine = {
+            dispatch: function (dispatch) {
+              this.dispatchWasCalled = true;
+              assert.equal(theDispatch, dispatch);
+            }
+          }
+
+          var triggeringPackage = new jstp.JSTPTriggeringPackage(mockEngine, "emitter");
+          triggeringPackage.dispatch(theDispatch);
+          assert.isTrue(mockEngine.dispatchWasCalled);
+        }
+      },
+
+      '( JSTPDispatch dispatch, JSTPCallable callback )': {
+        'should call the engine.dispatch with the arguments': function () {
+          var theDispatch = new jstp.JSTPDispatch();
+          var theCallback = function () {};
+
+          var mockEngine = {
+            dispatch: function (dispatch, callback) {
+              this.dispatchWasCalled = true;
+              assert.equal(theDispatch, dispatch);
+              assert.equal(theCallback, callback);
+            }
+          }
+
+          var triggeringPackage = new jstp.JSTPTriggeringPackage(mockEngine, "emitter");
+          triggeringPackage.dispatch(theDispatch, theCallback);
+          assert.isTrue(mockEngine.dispatchWasCalled);
+        },
+
+        'the callback has no .call method': {
+          'should raise a JSTPNotCallable error': function () {
+            var theDispatch = new jstp.JSTPDispatch();
+            var theCallback = "mockCallable";
+            var triggeringPackage = new jstp.JSTPTriggeringPackage(
+              {dispatch: function () {}}, "emitter");
+
+            assert.throws(function () {
+              triggeringPackage.dispatch(theDispatch, theCallback);
+            }, jstp.JSTPNotCallable);
+          }
+        }
+      },
+
+      '( JSTPDispatch dispatch, JSTPCallable callback, Object context )': {
+        'should call the engine.dispatch with the arguments': function () {
+          var theDispatch = new jstp.JSTPDispatch();
+          var theCallback = function () {};
+          var theContext  = {};
+
+          var mockEngine = {
+            dispatch: function (dispatch, callback, context) {
+              this.dispatchWasCalled = true;
+              assert.equal(theDispatch, dispatch);
+              assert.equal(theCallback, callback);
+              assert.equal(theContext,  context);
+            }
+          }
+
+          var triggeringPackage = new jstp.JSTPTriggeringPackage(mockEngine, "emitter");
+          triggeringPackage.dispatch(theDispatch, theCallback, theContext);
+          assert.isTrue(mockEngine.dispatchWasCalled);          
+        }
+      },
+
+    }
   }
 }).export(module); 
 
