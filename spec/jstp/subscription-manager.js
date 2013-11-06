19cac02373966ef2ac41791bf4e55c15e27766bc
Xavier Via <fernando.via@gmail.com>
2013-11-06 05:57:17 -0300

    JSTPSubscriptionManager: more in-depth spec

--- spec/jstp/subscription-manager.js
+++ spec/jstp/subscription-manager.js
@@ -3,7 +3,37 @@ var vows    = require('vows')
   , jstp    = require('../../index.js');
 
 vows.describe('JSTPSubscriptionManager').addBatch({
-  '#bind( JSTPDispatch dispatch, JSTPCallable callback, Object context )': 'pending',
-  '#release( JSTPDispatch dispatch, JSTPCallable callback, Object context )': 'pending',
+  '#bind( JSTPEndpoint endpoint, String transactionID, JSTPCallable callback [, Object context] )': {
+    'if not an JSTPEndpoint': {
+      'should throw an Error': 'pending'
+    },
+
+    'if the transactionID is absent': {
+      'should throw an Error': 'pending'
+    },
+
+    'if the callback has no #call': {
+      'should throw an Error': 'pending'
+    },
+
+    'if there is an endpoint already bound for that callback/context pair': {
+      'should throw a JSTPEndpointAlreadyBound exception': 'pending'
+    },
+
+    'if everything is ok': {
+      'should add the dictionary with the endpoint/callback/context/transactionID to the list': 'pending'
+    }
+  },
+
+  '#release( JSTPEndpoint endpoint, JSTPCallable callback, Object context )': {
+    'if was not found': {
+      'should throw a JSTPUnboundEndpoint exception': 'pending' 
+    },
+
+    'if it matched something': {
+      'should remove the matching dictionary from the list': 'pending'
+    }
+  },
+
   '#trigger( JSTPDispatch )': 'pending'
 }).export(module);     
