becd3bcd05db23c610bac426a272ee2766cf417b
Xavier Via <xavierviacanel@gmail.com>
2014-04-27 22:33:35 -0300

    Cleanup

--- spec/jstp/callable.js
+++
@@ -1,9 +0,0 @@
-var vows    = require('vows')
-  , assert  = require('assert')
-  , jstp    = require('../../index.js');
-
-vows.describe('JSTPCallable').addBatch({
-  '#call( JSTPEngine engine, JSTPTriggeringPackage triggeringPackage )': {
-    'should accept the arguments': function () { }
-  }
-}).export(module); 
